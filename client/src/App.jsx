dd654a6b631edaec0a7d2e9bbec2a8bf60888f84
Oshodi Alola <lilwade4000@gmail.com>
2025-10-21 03:04:59 +0100

    Tailwind + Client side routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+import WelcomePage from "./pages/Welcome/WelcomePage"
+import HomePage from "./pages/Home/HomePage"
+import NotFoundPage from "./pages/NotFoundPage"
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <WelcomePage />,
+      errorElement: <NotFoundPage />
+    },
+    {
+      path: '/home',
+      element: <HomePage />
+    }
+  ])
+
   return (
     <>
-     <div>Hiba and Alola's project</div>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ client/src/pages/Welcome/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import './WelcomePage.module.css'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1 className='font-bold text-3xl'>Welcome Page</h1>
+      <div className='font-medium'>Hiba and Alola's project</div>
+    </>
+  )
+}
+
+export default HomePage
