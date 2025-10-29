0aeab26809f13966a354a32ce022f0a9042e6c0d
Oshodi Alola <lilwade4000@gmail.com>
2025-10-29 22:35:33 +0100

    Minor fixes (Login page)

--- client/src/pages/Login/LogInPage.jsx
+++ client/src/pages/Login/LogInPage.jsx
@@ -1,7 +1,16 @@
 import { Link } from 'react-router-dom'
 import styles from './Login.module.css'
+import { useState } from 'react'
 
 const LoginInPage = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisible = () => {
+    setIsVisible(!isVisible);
+  };
+
+
   return (
     <>
       <div id="login-container" className={styles["login-container"]}>
@@ -31,16 +40,22 @@ const LoginInPage = () => {
           >Login</button>
 
           <div>
-            <input type="checkbox" id="remember-me"/>
+            <input type="checkbox" id="remember-me" onClick={toggleVisible}/>
             <label className='text-sm flex '> Remember these login details </label>
           </div>
           
-          <p className='text-[13px] ml-15'>
+          <p className='text-[13px] ml-12'>
             Not a member? <span className='font-bold text-[#932A98]'><Link to="/signup">Sign up</Link></span>
           </p>
         </form>
 
-        <p className='text-[13px] pb-15 ml-10 mr-10'>
+        { isVisible && (
+          <p id="remember-me-text" className='text-[13px] w-[70%] ml-6 opacity-80'>
+            You will be signed in with these details in subsequent sessions.
+          </p>
+        )}
+
+        <p className='text-[13px] pb-10 ml-10 mr-10 mt-8'>
       By signing up, you agree to our <span className='font-bold text-[#932A98]'>Terms of service</span> and
       <span className='font-bold text-[#932A98]'> Privacy Policy</span>
       </p>
