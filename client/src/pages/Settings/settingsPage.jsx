5882b382070ad404932a86c06c7b962c7fed7d5e
C-G <kelvindonye@gmail.com>
2025-11-13 20:13:14 +0100

    added-settingsPage

--- client/src/pages/Settings/settingsPage.jsx
+++ client/src/pages/Settings/settingsPage.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import "./settingsPage.module.css"
 import img from "../../assets/home-page/Ellipse 1.png"
+import { MdSwitchLeft, MdSwitchRight } from "react-icons/md";
+import { SwitchProvider } from '../../components/switchLight'; 
+import { useContext } from 'react';
 
 const SettingsPage = () => {
 
-  const  settingTabs = [
-    {
-      name: "View Details"
-    },
-    {
-      name: "Delete Account"
-    },
-    {
-      name: "Light Mode"
-    },
-    {
-      name: "Log out"
-    }
-  ]
+  const {switchLight, setSwitchLight} = useContext(SwitchProvider)
   
   return (
     <div className='place-self-center h-screen w-screen flex flex-col gap-4'>
@@ -30,13 +20,19 @@ const SettingsPage = () => {
           <p className=' text-[18px] self-end'>08064458326</p>
         </div>
       </div>
-      <div>
-        <ul className='w-full'>
-          {settingTabs.map(data => 
-            (<li className='border-t-2 border-[#932A98] h-20.5 flex justify-center items-center' key={data.name}>{data.name}</li>)
-          )}
-        </ul>
+      <div                                                                                                                                                                                                                                                                    >
+        <div className='border-t-2 flex items-center border-[#932A98] h-12 w-full p-10'>View Details</div>
+        <p className='border-t-2 flex items-center border-[#932A98] h-12 w-full '>Delete Account</p>
+        <div className='flex items-center gap-4 p-3 w-full pl-4 border-t-2 border-[#932A98] h-12 '>
+          <p>Light Mode</p>
+            <button onClick={() => setSwitchLight(prev => !prev)}>
+              { switchLight ? <MdSwitchLeft /> : <MdSwitchRight />}
+            </button>
+          <p>Dark Mode</p>
+        </div>
+        <p className='border-t-2 w-full pl-3 flex items-center justify-start border-b-2 border-[#932A98] h-12 '>Log out</p>
       </div>
+
     </div>
   )
 }
