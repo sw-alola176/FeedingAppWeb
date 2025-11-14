7fac56ef38aeddf10116cf915be3b0cfdd015ec8
C-G <kelvindonye@gmail.com>
2025-11-14 16:32:48 +0100

    fixed the menu button

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -9,22 +9,22 @@ const MenuBar = () => {
   const {showMenu, setShowMenu} = useContext(SwitchProvider)
 
   const homeActiveStyle = {
-    color: "white"
+    color: "#932A98"
   }
   const historyActiveStyle = {
-    color: "white"
+    color: "#932A98"
   }
   const settingsActiveStyle = {
-    color: "white"
+    color: "#932A98"
   }
 
   return (
     <div onClick={() => setShowMenu(prev => !prev)} className='flex items-center z-20 border-[#932A98] bottom-50 fixed'>
-        {showMenu ? <FaX className={`text-2xl text-white bg-[#932A98] w-12 h-12 border-[#932A98] p-2 rounded-3xl`} /> :
-        <FaList className='text-white bg-[#932A98] border-[#932A98] border-0 rounded-3xl text-2xl p-2 w-12 h-12' />}
+        {showMenu ? <FaX style={{padding: "10px"}} className={`text-[55px] text-white bg-[#932A98] border-[#932A98] rounded-3xl`} /> :
+        <FaList style={{padding: "10px"}} className='text-white bg-[#932A98] border-[#932A98] rounded-3xl text-[55px]' />}
         {showMenu ? (
-          <div className='bg-[#932A98] rounded-4xl p-2'>
-            <ul className='flex gap-4 text-[#C578C8E5]'>
+          <div style={{padding: "16px"}} className='bg-white shadow-[#932A98] shadow-2xs border-2 border-[#932A98] rounded-4xl h-10'>
+            <ul className='h-full flex items-center justify-center gap-4 text-sm text-[#C578C8E5]'>
               <li className='hover:text-[#932A98]'><NavLink style={({isActive}) => (isActive ? homeActiveStyle: null)} to='/home'>Home</NavLink></li>
               <li className='hover:text-[#932A98]'><NavLink style={({isActive}) => (isActive ? historyActiveStyle: null)} to='/history'>History</NavLink></li>
               <li className='hover:text-[#932A98]'><NavLink style={({isActive}) => (isActive ? settingsActiveStyle: null)} to='/settings'>Settings</NavLink></li>
