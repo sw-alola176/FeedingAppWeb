e11a3b058af3aa8078b8a6f46f3eab28dc14d324
C-G <kelvindonye@gmail.com>
2025-11-14 23:28:49 +0100

    cleaned up the code

--- client/src/pages/ForgotPassword/ForgotPasswordPage.jsx
+++ client/src/pages/ForgotPassword/ForgotPasswordPage.jsx
@@ -19,15 +19,11 @@ const ForgotPasswordPage = () => {
     
     if(password === newPassword){
       setNewChangedPassword(prev => !prev)
-    } else{
-      setWarning(prev => !prev)
-    }
-
-    if( password && newPassword === '' ){
+    }else if( password && newPassword === '' ){
       setNewChangedPassword(false)
-    }
-
-    
+    }else{
+      setWarning(prev => !prev)
+    }    
   }
 
   return (
@@ -38,32 +34,61 @@ const ForgotPasswordPage = () => {
           <p className='text-xl'>back</p>
         </Link>
       
-        <form onSubmit={handleChangePassword} className='mt-[25%] border-2 p-10 w-[85%] rounded-4xl border-[#922a9879] flex flex-col gap-10 self-center'>
+        <form 
+          onSubmit={handleChangePassword} 
+          className='mt-[25%] p-10 w-[85%] rounded-4xl flex-col gap-10 self-center'
+          >
+
           <h1 className='text-xl text-center text-[#932A98]'>Encryption password change</h1>
-          <p className='text-center'>Passwords must be at least <span className='text-[#932A98] font-bold'>eight</span> characters long</p>
+          <p className='text-center'>Passwords must be at least 
+            <span className='text-[#932A98] font-bold'>eight </span>
+            characters long
+          </p>
 
           <div>
             <label htmlFor='password'>New password</label>
-            <input id='password' name='password' type="password" required />
+            <input 
+              id='password' 
+              name='password' 
+              type="password" 
+              onInvalid={(e) => e.target.setCustomValidity("Please type in your password")} 
+              onInput={(e) => e.target.setCustomValidity('')}/>
           </div>
 
           <div>
             <label htmlFor='newPassword'>Confirm new password</label>
-            <input id='newPassword' name='newPassword' type="password" required />
+            <input 
+              id='newPassword' 
+              name='newPassword' 
+              type="password" 
+              required
+              onInvalid={(e) => e.target.setCustomValidity("Please type in your password")} 
+              onInput={(e) => e.target.setCustomValidity('')}/>
             {warning ? <p className='flex items-center gap-2 text-[#932A98] text-sm text-center'><RiErrorWarningLine /> Passwords must contain the same value</p>: null}
           </div>
 
-          <button disabled={setNewChangedPassword ? false :true} className='p-4 w-full rounded-2xl bg-[#932A98] text-white text-sm active:bg-[#922a9897] active:border-[#932A98] border-2' type="submit">Change Password</button>
+          <button 
+            disabled={setNewChangedPassword ? false : true} 
+            type="submit">Change Password
+          </button>
+
           {newChangedPassword ? <p>changed password</p>: null}
+
         </form>
         </>
         :
         <div className='flex flex-col gap-15 h-full text-center mt-[50%]'>
+
           <div className='bg-[#932A98] self-center w-fit p-6 rounded-full'>
             <FaCheck className='text-9xl text-white'/>
           </div>
+
           <p className='text-[#932A98] text-2xl w-full'>PASSWORD CHANGED SUCCESFULLY</p>
-          <button className='flex justify-center items-center gap-4 self-center p-4 w-[70%] rounded-2xl bg-[#932A98] text-white text-2xl active:bg-[#922a9897] active:border-[#932A98] border-2'>Next <FaArrowRightLong /></button>
+
+          <button 
+            className='flex justify-center items-center gap-4 self-center p-4 w-[70%]'>
+              Next <FaArrowRightLong />
+            </button>
         </div> 
         }
     </div>
